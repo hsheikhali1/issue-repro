69a21fb08b17ebcc695cd52059f7ca8683673c56
Harun Sheikhali <harun.sheikhali@agilebits.com>
2022-07-04 14:20:08 -0400

    issues to reproduce

---
+++ apps/website/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type {NextPage} from 'next';
+
+import {Cta} from 'vibranium-ui';
+
+const Home: NextPage = () => (
+	<div>
+		<h1>Hello World!</h1>
+		<Cta />
+	</div>
+);
+
+export default Home;
---
+++ lib/ui/src/components/atoms/Cta/Cta.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Cta = () => (
+	<a className='font-bold text-blue-500 bg-red-400' href='#'>
+    here is a simple cta
+	</a>
+);
